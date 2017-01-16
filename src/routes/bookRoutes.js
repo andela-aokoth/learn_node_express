cc67d0b761557bd227cf061d05feb15e160e8770
Arnold Okoth <arnold.okoth@andela.com>
2017-01-17 00:04:42 +0300

    Update book routes to use book controller

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -3,7 +3,9 @@ var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 var objectId = require('mongodb').ObjectID;
 
+
 var router = function(nav) {
+    // Use static list of books before integrating MongoDB
     var books = [
         {
             title: 'War and Peace',
@@ -31,45 +33,13 @@ var router = function(nav) {
         }
     ];
 
+    var bookController = require('../controllers/bookController')(null, nav);
+    bookRouter.use(bookController.middleware);
     bookRouter.route('/')
-              .get(function(req, res) {
-                  var url = 'mongodb://localhost:27017/libraryApp';
-                  mongodb.connect(url, function(url, db) {
-                    var collection = db.collection('books');
-                    collection.find({}).toArray(function(err, results) {
-                        if (err) {
-                            console.log(err.message);
-                        } else {
-                            res.render('bookListView', {
-                                title: 'Hello From Render!',
-                                nav: nav,
-                                books: results
-                            });
-                        }
-                    });
-                  });
-
-              });
+              .get(bookController.getIndex);
 
     bookRouter.route('/:id')
-              .get(function(req, res) {
-                  var id = new objectId(req.params.id);
-                  var url = 'mongodb://localhost:27017/libraryApp';
-                  mongodb.connect(url, function(url, db) {
-                      var collection = db.collection('books');
-                      collection.findOne({_id: id}, function(err, results) {
-                          if (err) {
-                              console.log(err.message);
-                          } else {
-                              res.render('bookView', {
-                                  title: 'Hello From Render!',
-                                  nav: nav,
-                                  books: results
-                              });
-                          }
-                      });
-                  });
-              });
+              .get(bookController.getById);
 
     return bookRouter;
 };
